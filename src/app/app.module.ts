e50122060a3bcc26b8fdfedb48ae8892011241be
Vane <primestone\vanessa.sora.ext>
2019-11-27 14:50:59 -0500

    upload components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ContentIndexComponent } from './componentes/content-index/content-index.component';
+import { FooterComponent } from './componentes/footer/footer.component';
+import { LoginComponent } from './componentes/login/login.component';
+import { MainMenuComponent } from './componentes/main-menu/main-menu.component';
+import { MenuSongComponent } from './componentes/menu-song/menu-song.component';
+import { PlayerComponent } from './componentes/player/player.component';
+import { SongToolsComponent } from './componentes/song-tools/song-tools.component';
+import { UserProfileComponent } from './componentes/user-profile/user-profile.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContentIndexComponent,
+    FooterComponent,
+    LoginComponent,
+    MainMenuComponent,
+    MenuSongComponent,
+    PlayerComponent,
+    SongToolsComponent,
+    UserProfileComponent
   ],
   imports: [
     BrowserModule
